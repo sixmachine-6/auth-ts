59f1b4f8d6a847266ad3295569ca71416535b8e2
Aman Shaw <shawaman2384@gmail.com>
2024-07-17 14:58:56 +0530

    auth-ts

--- build/decorators/bodyValidators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bodyValidators = bodyValidators;
-const Metadata_keys_1 = require("../enums/Metadata.keys");
-function bodyValidators(...keys) {
-    return function (target, key) {
-        Reflect.defineMetadata(Metadata_keys_1.MetadataKeys.validator, keys, target, key);
-    };
-}
